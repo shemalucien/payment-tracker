c0604059375a3b08224d21bef439a270dece8457
shemalucien <shemalucien5@gmail.com>
2024-04-05 11:03:07 +0200

    fixed multiselect

--- apps/web/src/components/Header/dropdown-message.tsx
+++ apps/web/src/components/Header/dropdown-message.tsx
@@ -22,7 +22,7 @@ const DropdownMessage = () => {
       setDropdownOpen(false);
     };
     document.addEventListener("click", clickHandler);
-    return () => document.removeEventListener("click", clickHandler);
+    return (): void => { document.removeEventListener("click", clickHandler); };
   });
 
   // close if the esc key is pressed
@@ -47,9 +47,8 @@ const DropdownMessage = () => {
         href="#"
       >
         <span
-          className={`absolute -right-0.5 -top-0.5 z-1 h-2 w-2 rounded-full bg-meta-1 ${
-            notifying === false ? "hidden" : "inline"
-          }`}
+          className={`absolute -right-0.5 -top-0.5 z-1 h-2 w-2 rounded-full bg-meta-1 ${notifying ? "hidden" : "inline"
+            }`}
         >
           <span className="absolute -z-1 inline-flex h-full w-full animate-ping rounded-full bg-meta-1 opacity-75"></span>
         </span>
@@ -84,11 +83,14 @@ const DropdownMessage = () => {
       {/* <!-- Dropdown Start --> */}
       <div
         ref={dropdown}
-        onFocus={() => setDropdownOpen(true)}
-        onBlur={() => setDropdownOpen(false)}
-        className={`absolute -right-16 mt-2.5 flex h-90 w-75 flex-col rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark sm:right-0 sm:w-80 ${
-          dropdownOpen === true ? "block" : "hidden"
-        }`}
+        onFocus={() => {
+          setDropdownOpen(true);
+        }}
+        onBlur={() => {
+          setDropdownOpen(false);
+        }}
+        className={`absolute -right-16 mt-2.5 flex h-90 w-75 flex-col rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark sm:right-0 sm:w-80 ${dropdownOpen ? "block" : "hidden"
+          }`}
       >
         <div className="px-4.5 py-3">
           <h5 className="text-sm font-medium text-bodydark2">Messages</h5>
